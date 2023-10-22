5efdc4fe79981547669428c2844baac6666d570f
bstawy <125200508@sha.edu.eg>
2023-10-23 02:26:10 +0300

    create a data model to store exercise data

---
+++ lib/model/exercise_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExerciseModel{
+  String title;
+  Color color;
+  String imagePath;
+
+  ExerciseModel({
+    required this.title,
+    required this.color,
+    required this.imagePath,
+  });
+}
